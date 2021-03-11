c9ce424f905dc472d49c124b3f8b84e1ddf39902
edsonrodsilva <edsonrodsilva@gmail.com>
2021-03-10 22:30:11 -0300

    created migrations

---
+++ database/migrations/2021_03_10_202701_create_clients_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->char('code', 20);
+            $table->string('name', 45);
+            $table->char('cpf', 11);
+            $table->string('email', 45);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
