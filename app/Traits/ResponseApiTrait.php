40429cf4ba83e509ac60f5ecce70e3099a68354d
edsonrodsilva <edsonrodsilva@gmail.com>
2021-03-11 13:09:52 -0300

    created trait for response to the client

---
+++ app/Traits/ResponseApiTrait.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Traits;
+
+trait ResponseApiTrait
+{
+    /**
+     * Core of response api
+     * @param       string              $message
+     * @param       array|object        $data
+     * @param       integer             $statusCode
+     * @param       boolean             $isSuccess
+     */
+    public function coreResponse($message, $data = null, $statusCode, $isSuccess = true)
+    {
+        if (!$message) return response()->json(['message' => 'Message id required'], 500);
+
+        //Send the response
+        if ($isSuccess) {
+            return response()->json([
+                'message' => $message,
+                'error' => false,
+                'code' => $statusCode,
+                'results' => $data
+            ], $statusCode);
+        } else {
+            return response()->json([
+                'message' => $message,
+                'error' => true,
+                'code' => $statusCode,
+            ], $statusCode);
+        }
+    }
+
+
+    /**
+     * Send any success response api
+     * @param       string              $message
+     * @param       array|object        $data
+     * @param       integer             $statusCode
+     */
+    public function success($message, $data, $statusCode = 200)
+    {
+        return $this->coreResponse($message, $data, $statusCode);
+    }
+
+    /**
+     * Send any error response api
+     * @param       string              $message
+     * @param       integer             $statusCode
+     */
+    public function error($message, $statusCode = 500)
+    {
+        return $this->coreResponse($message, null, $statusCode, false);
+    }
+}
