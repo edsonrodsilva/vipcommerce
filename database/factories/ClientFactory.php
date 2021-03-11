68579f677ea953380ade5dcd657a752faa644eb8
edsonrodsilva <edsonrodsilva@gmail.com>
2021-03-11 01:15:19 -0300

    created migrations tables and factory clients

---
+++ database/factories/ClientFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Client;
+use Faker\Provider\pt_BR\Person;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ClientFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Client::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        //Add provider Person
+        $this->faker->addProvider(new Person($this->faker));
+
+        return [
+            'code' => $this->faker->unique()->randomNumber(),
+            'name' => $this->faker->name,
+            'cpf' => $this->faker->cpf(false),
+            'email'  => $this->faker->safeEmail(),
+            'gender_id' => $this->faker->randomElement([1, 2])
+        ];
+    }
+}
